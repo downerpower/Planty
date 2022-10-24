03fb28830257b936a41fd90e4b36d98e6a5ab241
Мария Бейлина <marie.beilina3030@gmail.com>
2022-10-24 13:18:32 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <header className="headerLeft">
+        <div>
+          <img width={120} height={70} src='/img/logo.svg' alt='logo' />
+        </div>
+        <ul className="headerRight">
+          <li><img width={26} height={26} src='/img/shopping_cart.svg' alt='logo' /><span>1499 руб.</span></li>
+          <li><img width={26} height={26} src='/img/account_circle.svg' alt='logo' /></li>
+        </ul>
+      </header >
+      <div className="content">
+        <h1>Все растения</h1>
+        <div className="plants">
+          <div className="card">
+            <img width={100} height={112} src='/img/plants/monstera.png' alt='plants' />
+            <h5>Монстера</h5>
+            <div className="cardBottom">
+              <div className="cardDescription">
+                <span>Цена: </span>
+                <b>1499 руб.</b>
+              </div>
+              <div>
+                <button className="button favorite">
+                  <img width={32} height={32} src='/img/favorite.svg' alt='add' />
+                </button>
+                <button className="button">
+                  <img width={32} height={32} src='/img/add_box.svg' alt='add' />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <img width={100} height={112} src='/img/plants/succulent.png' alt='plants' />
+            <h5>Суккулент</h5>
+            <div className="cardBottom">
+              <div className="cardDescription">
+                <span>Цена: </span>
+                <b>749 руб.</b>
+              </div>
+              <div>
+                <button className="button favorite">
+                  <img width={32} height={32} src='/img/favorite.svg' alt='add' />
+                </button>
+                <button className="button">
+                  <img width={32} height={32} src='/img/add_box.svg' alt='add' />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <img width={100} height={112} src='/img/plants/kalanchoe.png' alt='plants' />
+            <h5>Каланхоэ</h5>
+            <div className="cardBottom">
+              <div className="cardDescription">
+                <span>Цена: </span>
+                <b>1399 руб.</b>
+              </div>
+              <div>
+                <button className="button favorite">
+                  <img width={32} height={32} src='/img/favorite.svg' alt='add' />
+                </button>
+                <button className="button">
+                  <img width={32} height={32} src='/img/add_box.svg' alt='add' />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <img width={100} height={112} src='/img/plants/sansevieria.png' alt='plants' />
+            <h5>Сансевиерия</h5>
+            <div className="cardBottom">
+              <div className="cardDescription">
+                <span>Цена: </span>
+                <b>899 руб.</b>
+              </div>
+              <div>
+                <button className="button favorite">
+                  <img width={32} height={32} src='/img/favorite.svg' alt='add' />
+                </button>
+                <button className="button">
+                  <img width={32} height={32} src='/img/add_box.svg' alt='add' />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
